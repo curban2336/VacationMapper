018314443b30dc84e7045fbd3a5cc85a8101b496
Chris Urban <curban841@gmail.com>
2026-04-05 15:18:37 -0400

    file organization

--- my-electron-app/map.js
+++ my-electron-app/map.js
@@ -14,8 +14,6 @@ app.whenReady().then(() => {
 })
 
 app.whenReady().then(() => {
-  createWindow()
-
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
